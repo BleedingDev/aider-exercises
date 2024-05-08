d425d6c1593e878f16a343a3c60a48acbdc636c5
Petr Glaser <syreanis+1@gmail.com>
2024-05-09 00:09:48 +0200

    feat(init): initialize repository to teach Aider

---
+++ index.js
@@ -0,0 +1,10 @@
+// Write welcome message to the console
+// Tell people that this is exercise to teach how to use Aider
+// I am AI consultant focused on boosting developers' productivity
+console.log("Welcome to Aider Exercises!");
+console.log("This is an exercise to teach you how to use Aider.");
+console.log(
+  "I am an AI consultant focused on boosting developers' productivity."
+);
+console.log("Find out more at GitHub: https://github.com/BleedingDev");
+console.log("Or LinkedIn: https://www.linkedin.com/in/bleedingdev/");
